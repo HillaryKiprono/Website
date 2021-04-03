64f8c78fc77b6aa6b487437ef7906ced956119f6
Hillary <kipronohillary240@gmail.com>
2021-04-03 17:16:52 +0300

    initial commit

---
+++ about.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+	<div class="p-3 mb-2 bg-success text-white">
+	<div class="container">
+	<nav>
+<ul class="nav justify-content-end " style="font-weight: 600">
+  <li class="nav-item p3">
+    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+  </li>
+  <li class="nav-item p3">
+    <a class="nav-link" href="about.php">About Us</a>
+  </li>
+  <li class="nav-item p3">
+    <a class="nav-link" href="contact.php">Contact Us</a>
+  </li>
+  <li class="nav-item p3">
+    <a class="nav-link" href="blog.php" tabindex="-1" aria-disabled="true">Blog</a>
+  </li>
+</ul>
+</nav>
+</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Website</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+	<div class="p-3 mb-2 bg-success text-white">
+	<div class="container">
+	<nav>
+<ul class="nav justify-content-end " style="font-weight: 600">
+  <li class="nav-item p3">
+    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+  </li>
+  <li class="nav-item p3">
+    <a class="nav-link" href="about.php">About Us</a>
+  </li>
+  <li class="nav-item p3">
+    <a class="nav-link" href="contact.php">Contact Us</a>
+  </li>
+  <li class="nav-item p3">
+    <a class="nav-link" href="blog.php" tabindex="-1" aria-disabled="true">Blog</a>
+  </li>
+</ul>
+</nav>
+</div>
+</div>
+<h3> Explore Goodies</h3>
+<div class="carousel-inner">
+			<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-item active">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjV2cggf4ndCWTU8rCDrYJRYVzPdt0b_-o8tWu0fCzinfK53tIxBaWSm5PlKz8K2UEXes&usqp=CAU" class="d-block w-100" alt="..." width="100%" height="350px">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+<hr width="100%">
+<div class="container">
+	<div class="row">
+		<div class="col-3">
+			<div class="card" style="">
+				<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjV2cggf4ndCWTU8rCDrYJRYVzPdt0b_-o8tWu0fCzinfK53tIxBaWSm5PlKz8K2UEXes&usqp=CAU" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+  <div class="card-body">
+    <h5 class="card-title">Pizza</h5>
+    <p class="card-text">Get you favorite pizza </p>
+    <a href="#" class="btn btn-primary">Order Now</a>
+  </div>
+</div>
+</div>
+	<div class="col-3">
+			<div class="card" style="">
+
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+	<div class="col-3">
+			<div class="card" style="">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+	<div class="col-3">
+			<div class="card" style="">
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
